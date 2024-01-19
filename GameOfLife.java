03cb63ee5d1d263aab3fc29f299c5b0898af965f
Shay Levy <shay.levy01@post.runi.ac.il>
2024-01-19 23:39:02 +0200

    GOL4

--- GameOfLife.java
+++ GameOfLife.java
@@ -155,7 +155,7 @@ public class GameOfLife {
     public static void print(int[][] arr) {
 		for (int i = 1 ; i < arr.length-1 ; i++){
 			for (int j = 1 ; j < arr.length-1 ; j++){
-				System.out.printf(" %2s", arr[i][j]);
+				System.out.printf(" %3s", arr[i][j]);
 			}
 			System.out.println();
 		}
